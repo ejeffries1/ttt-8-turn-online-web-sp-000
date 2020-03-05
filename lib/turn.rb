51a194043d847697c4cdbbd00e33256886eb23ca
Evan Jeffries <ejeffries14@gmail.com>
2020-03-05 06:23:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
-    move(board, index, token)
+    move(board, index, token = "X")
   else
     turn
   end
